e237a23f059aae7ef35e63312f703d44cdce2fb0
Mithun-18 <mithunpoojary180@gmail.com>
2024-03-02 22:51:27 +0530

    minor change

--- src/router.js
+++ src/router.js
@@ -2,12 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateBoardScreen from "./screens/create_board_screen";
 import LoginScreen from "./screens/login_screen";
 import ManageTasksScreen from "./screens/manage_tasks_screen";
-import EmptyState from "./component/common/emptyState";
-import SelectBoard from "./component/common/selectBoard";
-import { CustomModal } from "./component/core/custom_modal";
-import AddTask from "./component/core/custom_modal/components/addTask";
-import NewTaskBoard from "./screens/create_board_screen/components/newTaskBoard";
-import TaskCard from "./screens/manage_tasks_screen/components/taskCard";
 
 export default () => {
   return (
@@ -15,14 +9,9 @@ export default () => {
       <BrowserRouter>
         <Routes>
           <Route
-            path="/temp-modal"
-            element={
-              <CustomModal>
-                <AddTask />
-              </CustomModal>
-            }
+            path="/manage-task"
+            element={<ManageTasksScreen></ManageTasksScreen>}
           />
-          <Route path="/" element={<TaskCard heading={'heading'} description={'des'}/>} />
           <Route path="/create-board" element={<CreateBoardScreen />} />
           <Route path="/login" element={<LoginScreen />} />
         </Routes>
